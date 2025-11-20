c8a46d2015bf29c9ec8e6f8024f0d517a8be756a
Dharshana S <dharshanans54@gmail.com>
2025-11-20 23:09:39 +0530

    examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import Header from './components/Header.jsx';
+import Header from './components/Header/Header.jsx';
 import { CORE_CONCEPTS } from './data.js.js';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 
 function App() {
+  let tabContent = 'Please  click a button'
+  function handleSelect(selectedButton){
+    tabContent = selectedButton;
+    console.log(tabContent);
+    
+     //selectedButton => 'components, 'jsx', 'props','state'
+        console.log('Hello World -Selected', selectedButton);
+
+    }
   return (
     <div>
       <Header />
@@ -17,6 +27,16 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[1]}/>
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} /></ul></section>
+            <section id="examples">
+              <h2>Examples</h2>
+              <menu>
+                <TabButton label="Components" onSelect={()=>handleSelect('components')}></TabButton>
+                <TabButton label="JSX" onSelect={()=>handleSelect('jsx')}></TabButton>
+                <TabButton label="Props" onSelect={()=>handleSelect('props')}></TabButton>
+                <TabButton label="State" onSelect={()=>handleSelect('state')}></TabButton>
+                </menu>
+            {tabContent}
+            </section>
 
       </main>
     </div>
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,8 @@
+export default function TabButton({ label, onSelect}) {
+    
+    return (
+        <li> 
+        <button onClick={onSelect}>{label}</button>
+        </li>
+    )
+}
