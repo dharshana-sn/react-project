21141b2e1d229aa2b0719298ae149c458d6a9f99
Dharshana S <dharshanans54@gmail.com>
2025-11-20 20:30:03 +0530

    Folder Structure

---
+++ src/components/CoreConcept.jsx
@@ -0,0 +1,11 @@
+
+
+export default function CoreConcept({image,title,description}) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <h4>{ description}</h4>
+    </li>
+  )
+}
